2180b3396e13211714f46b435f52798b38098977
Elxse <elise.chin@hotmail.fr>
2020-04-13 14:00:32 +0200

    meilleur modele kNN

--- modules/knn.py
+++ modules/knn.py
@@ -1,4 +1,4 @@
-# k Nearest Neighbors
+# K Nearest Neighbors
 
 import numpy as np
 import pandas as pd
@@ -7,22 +7,16 @@ import random as rd
 from math import sqrt
 from collections import defaultdict, Counter
 
-###############
-# Metrics
-###############
-
 def minkowski(x, y, p):
-    """[summary]
-    If p = 1, manhattan distance
-    If p = 2, euclidean distance
+    """Minkowski distance.
 
     Arguments:
-        x {[type]} -- [description]
-        y {[type]} -- [description]
-        p {[type]} -- [description]
+        x {list or numpy-ndarray} -- first observation
+        y {list or numpy-ndarray} -- second observation
+        p {int} -- power parameter for the Minkowski metric. When p = 1, this is equivalent to using manhattan_distance (l1), and euclidean_distance (l2) for p = 2. For arbitrary p, minkowski_distance (l_p) is used.
     
     Returns:
-        [type] -- [description]
+        float -- Minkowski distance of power p between x and y
     """
     if(p == "+infty"):
         return np.max(x-y)
@@ -30,19 +24,18 @@ def minkowski(x, y, p):
         return np.min(x-y)
     return np.power(np.sum(np.abs(np.power(x - y, p))), 1/p)
 
-def SquD(x, y):
-    return np.sum(np.power(x - y, 2)/(x + y))
-
-class KNeighborsClassifier():
-    """
+class OurKNeighborsClassifier():
+    """Classifier implementing the k-nearest neighbors vote.
 
     Arguments:
-        metric {}
+        n_neighbors {int} -- number of neighbors to find for a point
+        metric {callable} -- the distance metric to use
+        p {int} -- power parameter for the Minkowski metric. When p = 1, this is equivalent to using manhattan_distance (l1), and euclidean_distance (l2) for p = 2. For arbitrary p, minkowski_distance (l_p) is used.
 
     Attributes:
-        X_train_
-        y_train_
-        score_
+        X_train_ {numpy-ndarray} of shape (n_samples, n_features) -- the training input 
+        y_train_ {numpy-ndarray} of shape (n_samples, ) -- the classes labels
+        score_ {float} -- the accuracy score of the model
     
     """
     def __init__(self, n_neighbors=1, metric=minkowski, p=2):
@@ -80,6 +73,15 @@ class KNeighborsClassifier():
         return self
 
     def fit(self, X, y):
+        """Fit the model using X as training data and y as target values.
+        
+        Arguments:
+            X {numpy_ndarray} of shape (n_samples, n_features) -- the training input samples without labels
+            y {numpy_ndarray} of shape (n_samples,) -- the classes labels
+        
+        Returns:
+            OurKNeighborsClassifier -- self
+        """
         if self.n_neighbors > X.shape[0]:
             raise ValueError("Invalid argument: k can't be larger than number of samples.")
 
@@ -87,90 +89,61 @@ class KNeighborsClassifier():
         self.y_train_ = y
         return self
 
-    def predict(self, X):
+    def kneighbors(self, x, n_neighbors):
+        """Finds the k-neighbors of a point.
         
+        Arguments:
+            x {list or numpy-ndarray} of shape (n_samples, ) -- a point
+            n_neighbors {int} -- number of neighbors to find for x
+        
+        Returns:
+            list of float, list of int -- the n_neighbors nearest neighbors distances from x and their indexes
+        """
+        neigh_distances = []
+        neigh_indexes = []
+
+        # Loop through each training point
+        for i in range(self.X_train_.shape[0]):
+
+            # Compute and store distance between the training point and the observation
+            if(self.metric == minkowski):
+                neigh_distances.append([self.metric(x, self.X_train_[i,:], self.p), i])
+            else:
+                neigh_distances.append([self.metric(x, self.X_train_[i,:]), i])
+        
+        # Sort the list
+        neigh_distances = sorted(neigh_distances)
+
+        # Make a list of the k neighbors' indexes
+        for k in range(self.n_neighbors):
+            index = neigh_distances[k][1]
+            neigh_indexes.append(index)
+        
+        return neigh_distances, neigh_indexes
+
+
+    def predict(self, X):
+        """Predict class for X.
+    
+        Arguments:
+            X {numpy-ndarray} of shape (n_samples, n_features) -- the test input to classify
+        
+        Returns:
+            list -- the predicted classes
+        """
         predictions = []
         
         # Loop through each observation
         for i in range(X.shape[0]):
-            x_distances = []
             x_predictions = []
 
-            # Loop through each training point
-            for j in range(self.X_train_.shape[0]):
-                # Compute and store distance between the training point and the observation
-                if(self.metric == minkowski):
-                    x_distances.append([self.metric(X[i], self.X_train_[j,:], self.p), j])
-                else:
-                    x_distances.append([self.metric(X[i], self.X_train_[j,:]), j])
+            # Get the k nearest neighbors indexes of each observation
+            _, neigh_indexes = self.kneighbors(X[i,:], self.n_neighbors)
             
-            # Sort the list
-            x_distances = sorted(x_distances)
-
             # Make a list of the k neighbors' labels
-            for k in range(self.n_neighbors):
-                index = x_distances[k][1]
-                x_predictions.append(self.y_train_[index])
-            
+            x_predictions = [self.y_train_[index] for index in neigh_indexes]
+
             # Save the most common label
             predictions.append(Counter(x_predictions).most_common(1)[0][0])
 
         return predictions
-
-    def score(self, guessed_labels, true_labels):
-        """Computes the accuracy of the random forest.
-        
-        Arguments:
-            guessed_labels {list} -- the list of labels guessed by the decision tree
-            true_labels {list} -- the list of the real labels
-        
-        Returns:
-            float -- the accuracy
-        """
-        score = 0
-        n = len(guessed_labels)
-        for i in range(n):
-            score += (int(guessed_labels[i]) == int(true_labels[i]))
-        score = score/n
-        return score
-
-    def findFeatureImportance(self, X, y):
-        """Calculate feature importances according to score function of OurDecisionTreeClassifier.
-
-        Arguments:
-            X {numpy-ndarray} of shape (n_samples, n_features) -- the training dataset without labels
-            y {numpy-ndarray} of shape (n_samples,) -- the labels of the training dataset
-
-        Returns:
-            tuple of list -- the features associated with their importance
-        """
-
-        n_features = X.shape[1]
-        feat_imp = {}
-
-        # Make predictions
-        predictions_without_shuffle = self.predict(X)
-
-        # Compute the error value with all columns in place without shuffling
-        base = self.score(predictions_without_shuffle, y)
-
-        # Loop through each column 
-        for ind in range(n_features):
-            print("ind = ", ind)
-            X_new = X.copy()
-
-            # Shuffle the column
-            np.random.shuffle(X_new[:, ind])
-
-            # Make predictions again
-            predictions_with_shuffle = self.predict(X_new)
-
-            # Compute change in error term as compared to predictions_without_shuffle
-            # Greater change means more importance 
-            feat_imp[ind] = abs(self.score(predictions_with_shuffle, y) - base)
-
-            del X_new
-
-        features, importances = zip(*feat_imp.items())
-        return features, importances
-
